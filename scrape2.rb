9f91309d34462106deb17d6ca36677865a89e92f
DrewPrentice <weirdpercent@gmail.com>
2013-10-02 18:20:25 -0400

    one-item arrays to strings for JSON

--- scrape2.rb
+++ scrape2.rb
@@ -1,7 +1,7 @@
 require 'json/add/core'
 require 'json/pure'
 require 'metainspector'
-class Parsed
+class Parsed #The dreaded CSS selector class! Why did KVR have to use images?!
   def win(pdoc)
     pdoc.at_css(".sp1b")
   end
@@ -71,7 +71,7 @@ class Txt
     ptext =~ /Can HostEffects/
   end
 end
-class Plinko
+class Pachinko #Ever played Pachinko?
   def type(pdoc, format)
     parsed=Parsed.new
     if parsed.vst(pdoc) != nil then format.push "VST"; end
@@ -115,24 +115,24 @@ while x <= y
   cando=[]
   os=[]
   query=plinks[x].chomp
-  kvr=MetaInspector.new(query)
+  kvr=MetaInspector.new(query) #this data needs alot of work
   pdoc=kvr.parsed_document
   ptext=pdoc.text
-  plinko=Plinko.new
-  plinko.type(pdoc, format)
-  plinko.platform(pdoc, os)
-  plinko.function(ptext, cando)
-  hash=kvr.to_hash #great start, but it needs alot of work
+  pachinko=Pachinko.new
+  pachinko.type(pdoc, format)     #100 lines
+  pachinko.platform(pdoc, os)     #of code
+  pachinko.function(ptext, cando) #in 3 method calls
+  hash=kvr.to_hash
   hash.delete('title')
   hash.delete('links')
   hash.delete('internal_links')
   hash.delete('images')
   hash.delete('charset')
   hash.delete('feed')
-  hash.delete('content_type') #see what I mean?
+  hash.delete('content_type')
   meta=hash['meta']
   hash.delete('meta')
-  name=meta['name'] #now these elements are accessible
+  name=meta['name']
   name.delete('description')
   name.delete('og:image')
   name.delete('msapplication_tilecolor')
@@ -141,25 +141,25 @@ while x <= y
   extr=extr.reject {|url| url =~ /KVR-Audio/}
   extr=extr.reject {|url| url =~ /kvraudio/}
   hash['external_links']=extr
-  meta=hash.merge(name) #and all elements together
+  meta=hash.merge(name)
   meta['link']=meta['url']
   meta['plink']=meta['external_links']
   meta['tags']=meta['keywords']
   keyw=meta['tags'].split(', ') #now only array in hash
   meta['tags']=keyw
-  meta['atitle']=meta['og:title'] 
+  meta['atitle']=meta['og:title']
   ntitle=meta['atitle'].gsub(/ -  Details/, '')
   dsplit=ntitle.split(/ by /) #take values and leave the rest
   meta['atitle']=dsplit[0].to_s
   meta['dev']=dsplit[1].to_s #new pair
-  about=pdoc.at_css('.kvrproductimages').parent.text #Nokogiri hack
+  about=pdoc.at_css('.kvrproductimages').parent.text #better summary
   meta['summary']=about #replace with complete product info
   meta.delete('url')
   meta.delete('external_links')
   meta.delete('keywords')
   meta.delete('og:title')
   meta.delete('og:description') #ditch original keys
-  meta=meta.sort #meta is now an array of arrays, that won't do
+  meta=meta.sort #meta is now an array of arrays, unacceptable!
   atitle=meta.assoc('atitle')
   dev=meta.assoc('dev')
   link=meta.assoc('link')
@@ -167,13 +167,43 @@ while x <= y
   summary=meta.assoc('summary')
   tags=meta.assoc('tags') #extract each from array
   newhash={}
-  newhash['atitle']=atitle[1] #nice clean hash to_json
-  if cando != [] then newhash['cando']=cando; end
+  newhash['atitle']=atitle[1] #assemble clean hash for JSON
+  if cando.length > 1
+    newhash['cando']=cando
+  elsif cando.length == 1
+    cando=cando[0].to_s
+    newhash['cando']=cando
+  else
+    newhash['cando']=''
+  end
   newhash['dev']=dev[1]
-  if format != [] then newhash['format']=format; end
+  if format.length > 1
+    newhash['format']=format
+  elsif format.length == 1
+    format=format[0].to_s
+    newhash['format']=format
+  else
+    newhash['format']=''
+  end
   newhash['link']=link[1]
-  if os != [] then newhash['os']=os; end
+  if os.length > 1 #so many conditions...
+    newhash['os']=os
+  elsif os.length == 1
+    os=os[0].to_s
+    newhash['os']=os
+  else
+    newhash['os']=''
+  end
+  ple=plink[1]
+  if ple.length == 1
+    ple=ple[0].to_s
+  end
+  plink[1]=ple
   newhash['plink']=plink[1]
+  se=summary[1]
+  se=se.gsub(/^\s*/, '')
+  se=se.gsub(/\s*$/, '') #strip beginning and ending whitespace
+  summary[1]=se
   newhash['summary']=summary[1]
   newhash['tags']=tags[1] #ugh, that was an ordeal
   tofile=JSON.pretty_generate(newhash) #make it more readable
