1f529bdeae5e0f5de2b66d9ce45473accfacd540
Drew Prentice <weirdpercent@gmail.com>
2014-01-12 03:22:03 -0500

    update gem dep

--- scrape1.rb
+++ scrape1.rb
@@ -27,13 +27,13 @@ class First
         plinks.push x
       end
     end
-    filename="plinks.txt"
-    #filename="alllinks.txt" #write to big list of 2500+ products
-    if File.exist?(filename) == true
-      file=File.open(filename, "a")
-    else
-      file=File.new(filename, "w+")
-    end
+    #filename="plinks.txt"
+    filename="alllinks.txt" #write to big list of 2500+ products
+    #if File.exist?(filename) == true
+      file=File.open(filename, "w+")
+    #else
+      #file=File.new(filename, "w+")
+    #end
     plinks.each do |x|
       prefix=x.gsub(/http:\/\/www.kvraudio.com\/product\//, "") # strip URL before product name
       suffix=prefix.gsub(/-by-\w*/, "") # strip developer
